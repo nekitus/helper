f3aa186b7d35ddced696ebcb013ef0c079e2cb5b
nekitus <nikita-8787@mail.ru>
2014-07-09 23:38:24 +0400

    fix

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,9 +1,9 @@
 import java.lang.System;
 
-public static class Tasks {
+public class Tasks {
     public static String Some (){
         System.out.println("sdf");
-
+        return "";
     }
 
 }
